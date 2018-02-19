4c99739dfec4e3d7df347b408b1ae64757599567
Oscar Bonilla Rodríguez <docs@MacBook-Pro-de-Oscar.local>
2018-02-18 18:31:15 -0600

    login page

---
+++ deleteme/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,25 @@
+﻿
+//sive para encoontrar la main view model
+namespace deleteme.Infrastructure
+{
+    using ViewModels;
+    public class InstanceLocator
+    {
+        #region properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+
+        #region constructor
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+
+    }
+}
---
+++ deleteme/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+/*
+ * la clase main view model es la clase que va a controlar a todas las hijas
+ */
+namespace deleteme.ViewModels
+{
+    public class MainViewModel
+    {
+
+    }
+}
